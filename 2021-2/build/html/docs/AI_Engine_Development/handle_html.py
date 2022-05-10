3308dd085fd7db2cb44c0aa738a06cd0dbf98f61
tzx0004 <103074459+tzx0004@users.noreply.github.com>
2022-05-10 10:41:53 -0600

    Update handle_html.py

--- 2021-2/build/html/docs/AI_Engine_Development/handle_html.py
+++ 2021-2/build/html/docs/AI_Engine_Development/handle_html.py
@@ -1,4 +1,6 @@
 import sys,os
+import requests
+import zipfile
 dirname = os.path.dirname(__file__)
 print(dirname)
 from bs4 import BeautifulSoup
@@ -46,4 +48,28 @@ for root, dirnames, filenames in os.walk(directory):
             handle_html(fname)
 
 print("HERE")
+
+
+
+#server_url = "https://xilinx-staging.fluidtopics.net/api/admin/khub/sources/ftml/upload"
+server_url = 'https://docs.xilinx.com/api/admin/khub/sources/ftml/upload'
+HEADERS = {'FT-Authorization': 'Bearer NRcvr7KGYsuYmd6KELscCnMQlA6oLmog' }
+print("In uploading to FT")
+zf = zipfile.ZipFile('techdocs.zip', "w")
+for dirname, subdirs, files in os.walk("."):
+    print("dirname " + dirname)
+    zf.write(dirname)
+    for filename in files:
+        print("filename " +filename)
+        zf.write(os.path.join(dirname, filename))
+zf.close()
+  
+fileobj = open ('techdocs.zip', 'rb')
+r = requests.post(server_url, headers=HEADERS, files={"archive": ('techdocs.zip', fileobj)})
+#r = requests.get(server_url +"/api/khub/maps" , headers=HEADERS)
+
+if r.ok:
+    print("OK")
+else:
+    print(r.status_code)
             
