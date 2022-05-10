526435cf085862dcafb5a132da41dbfdc2b25f40
tzx0004 <103074459+tzx0004@users.noreply.github.com>
2022-05-10 09:56:41 -0600

    Create upload_to_ft.py

---
+++ 2021-2/build/html/docs/AI_Engine_Development/upload_to_ft.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import requests
+import zipfile
+
+#server_url = "https://xilinx-staging.fluidtopics.net/api/admin/khub/sources/ftml/upload"
+server_url = 'https://docs.xilinx.com/api/admin/khub/sources/ftml/upload'
+HEADERS = {'FT-Authorization': 'Bearer NRcvr7KGYsuYmd6KELscCnMQlA6oLmog' }
+
+zf = zipfile.ZipFile('techdocs.zip', "w")
+for dirname, subdirs, files in os.walk("."):
+    zf.write(dirname)
+    for filename in files:
+        print(filename)
+        zf.write(os.path.join(dirname, filename))
+zf.close()
+  
+fileobj = open ('techdocs.zip', 'rb')
+r = requests.post(server_url, headers=HEADERS, files={"archive": ('techdocs.zip', fileobj)})
+#r = requests.get(server_url +"/api/khub/maps" , headers=HEADERS)
+
+if r.ok:
+    print("OK")
+else:
+    print(r.status_code)
