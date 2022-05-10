8b464bec966103f6661d4e921034689a21d27e3e
tzx0004 <103074459+tzx0004@users.noreply.github.com>
2022-05-10 10:46:16 -0600

    Update handle_html.py

--- 2021-2/build/html/docs/AI_Engine_Development/handle_html.py
+++ 2021-2/build/html/docs/AI_Engine_Development/handle_html.py
@@ -51,25 +51,24 @@ print("HERE")
 
 
 
-#server_url = "https://xilinx-staging.fluidtopics.net/api/admin/khub/sources/ftml/upload"
-server_url = 'https://docs.xilinx.com/api/admin/khub/sources/ftml/upload'
-HEADERS = {'FT-Authorization': 'Bearer NRcvr7KGYsuYmd6KELscCnMQlA6oLmog' }
+#server_url = 'https://docs.xilinx.com/api/admin/khub/sources/ftml/upload'
+#HEADERS = {'FT-Authorization': 'Bearer NRcvr7KGYsuYmd6KELscCnMQlA6oLmog' }
 print("In uploading to FT")
-zf = zipfile.ZipFile('techdocs.zip', "w")
-for dirname, subdirs, files in os.walk("."):
-    print("dirname " + dirname)
-    zf.write(dirname)
-    for filename in files:
-        print("filename " +filename)
-        zf.write(os.path.join(dirname, filename))
-zf.close()
+#zf = zipfile.ZipFile('techdocs.zip', "w")
+#for dirname, subdirs, files in os.walk("."):
+#    print("dirname " + dirname)
+#    zf.write(dirname)
+#    for filename in files:
+#        print("filename " +filename)
+#        zf.write(os.path.join(dirname, filename))
+#zf.close()
   
-fileobj = open ('techdocs.zip', 'rb')
-r = requests.post(server_url, headers=HEADERS, files={"archive": ('techdocs.zip', fileobj)})
+#fileobj = open ('techdocs.zip', 'rb')
+#r = requests.post(server_url, headers=HEADERS, files={"archive": ('techdocs.zip', fileobj)})
 #r = requests.get(server_url +"/api/khub/maps" , headers=HEADERS)
 
-if r.ok:
-    print("OK")
-else:
-    print(r.status_code)
+#if r.ok:
+#    print("OK")
+#else:
+#    print(r.status_code)
             
