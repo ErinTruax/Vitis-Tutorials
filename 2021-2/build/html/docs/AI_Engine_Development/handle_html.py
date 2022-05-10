cce24aa74840bf7d784e48e3b1bfe83aaca1d924
tzx0004 <103074459+tzx0004@users.noreply.github.com>
2022-05-10 10:52:47 -0600

    Update handle_html.py

--- 2021-2/build/html/docs/AI_Engine_Development/handle_html.py
+++ 2021-2/build/html/docs/AI_Engine_Development/handle_html.py
@@ -52,16 +52,16 @@ print("HERE")
 
 
 server_url = 'https://docs.xilinx.com/api/admin/khub/sources/ftml/upload'
-HEADERS = {'FT-Authorization': 'Bearer NRcvr7KGYsuYmd6KELscCnMQlA6oLmog' }
+headers = {'FT-Authorization': 'Bearer NRcvr7KGYsuYmd6KELscCnMQlA6oLmog' }
 print("In uploading to FT")
-zf = zipfile.ZipFile('techdocs.zip', "w")
-for dirname, subdirs, files in os.walk("."):
-    print("dirname " + dirname)
-    zf.write(dirname)
-    for filename in files:
-        print("filename " +filename)
-        zf.write(os.path.join(dirname, filename))
-zf.close()
+#zf = zipfile.ZipFile('AI_Engine.zip', "w")
+#for dirname, subdirs, files in os.walk("."):
+#    print("dirname " + dirname)
+#    zf.write(dirname)
+#    for filename in files:
+#        print("filename " +filename)
+#        zf.write(os.path.join(dirname, filename))
+#zf.close()
   
 #fileobj = open ('techdocs.zip', 'rb')
 #r = requests.post(server_url, headers=HEADERS, files={"archive": ('techdocs.zip', fileobj)})
