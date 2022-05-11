bfc0e6cca71dcc4520d277f6265858774e1c1c2d
tzx0004 <103074459+tzx0004@users.noreply.github.com>
2022-05-10 19:46:08 -0600

    Update upload_to_ft.py

--- 2021-2/build/html/docs/AI_Engine_Development/upload_to_ft.py
+++ 2021-2/build/html/docs/AI_Engine_Development/upload_to_ft.py
@@ -7,16 +7,9 @@ import zipfile
 server_url = 'https://docs.xilinx.com/api/admin/khub/sources/ftml/upload'
 HEADERS = {'FT-Authorization': 'Bearer NRcvr7KGYsuYmd6KELscCnMQlA6oLmog' }
 print("In uploading to FT")
-zf = zipfile.ZipFile('techdocs.zip', "w")
-for dirname, subdirs, files in os.walk("."):
-    print("dirname " + dirname)
-    zf.write(dirname)
-    for filename in files:
-        print("filename " +filename)
-        zf.write(os.path.join(dirname, filename))
-zf.close()
+
   
-fileobj = open ('techdocs.zip', 'rb')
+fileobj = open ('AI.zip', 'rb')
 r = requests.post(server_url, headers=HEADERS, files={"archive": ('techdocs.zip', fileobj)})
 #r = requests.get(server_url +"/api/khub/maps" , headers=HEADERS)
 
